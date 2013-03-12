f137ec1163ba5ad38a5d757bb31789f064e515ef
Valentin Niculae <blade150388@yahoo.com>
2013-03-12 17:07:58 +0200

    Added methods and worked on structure

--- js/jquery.LuLuSlider.js
+++ js/jquery.LuLuSlider.js
@@ -4,8 +4,7 @@
 
         var settings    = $.extend( {
             loadButtons : false,
-            navButtons  : 'nav-buttons',
-            navArrows   : false
+            navButtons  : '.nav-buttons'
         }, options);
 
         settings.current        = 0;
@@ -13,83 +12,80 @@
         settings.elementWidth   = this.find('li').outerWidth(true);
         settings.$slider        = this;
 
+        var methods = {
+            init: function(){
 
-        settings.$slider.find('.nav-arrows a').bind('click', function(){
-            ($(this).hasClass('prev')) ? settings.current-- : settings.current++;
-            goTo(settings.current);
-            return false;
-        });
-
-        settings.$slider.on('click', '.nav-buttons span', function(){
-            goTo($(this).data('slide'));
-        });
-
-        if(settings.navButtons){
-            createNavButtons();
-            markActive(settings.current);
-        }
-
-        // Bind load on a button if is passed in options
-        if(settings.loadButtons){
-            $(settings.loadButtons).bind('click', function(){
-                loadSlides( $(this).attr('href'), createNavButtons );
-                return false;
-            });
-        }
-
-        function markActive(slide){
-            settings.$slider.find('.nav-buttons span').removeClass('active');
-            settings.$slider.find('.nav-buttons span').eq(slide).addClass('active');
-        }
-
-        //
-        function goTo(slide){
-            console.log(slide);
-            if(slide >= settings.total){
-                settings.current = 0;
-            }else if(slide < 0 ){
-                settings.current = settings.total - 1;
-            }else{
-                settings.current = slide;
-            }
-
-            markActive(settings.current);
-            settings.$slider.find('ul').animate({ marginLeft : -settings.current*settings.elementWidth})
-        }
-
-        // Create bullets nav buttons
-        function createNavButtons(){
-            var elements = settings.$slider.find('li');
-
-            if( settings.$slider.find( '.' + settings.navButtons).length == 0 ){
-                $('<div></div>',{
-                    class: settings.navButtons
-                }).appendTo(settings.$slider);
-            }
-
-            settings.$slider.find( '.' + settings.navButtons).empty();
-
-            elements.each(function( index ) {
-                $('<span data-slide="' + index + '"></span>').appendTo( settings.$slider.find(' .nav-buttons') );
-            });
-
-            goTo(0);
-        }
-
-
-        // Load slides via ajax
-        function loadSlides(url, callback){
-            $.ajax({
-                type: "GET",
-                url: url,
-                cache: false
-            }).success(function( response ) {
-                settings.$slider.find('ul').fadeOut(function() {
-                    $(this).empty().append( response ).fadeIn();
-                    settings.total = settings.$slider.find('li').length;
-                    callback();
+                // Bind navigation arrows
+                settings.$slider.find('.nav-arrows a').bind('click', function() {
+                    ( $(this).hasClass('prev') ) ? settings.current-- : settings.current++;
+                    methods.goTo(settings.current);
+                    return false;
                 });
-            });
+
+                // Bind nav buttons 
+                settings.$slider.on('click', '.nav-buttons span', function() {
+                    methods.goTo( $(this).data('slide') );
+                });
+
+                // Bind ajax load buttons
+                if(settings.loadButtons) {
+                    $(settings.loadButtons).bind('click', function(){
+                        methods.load( $(this).attr('href'), methods.createNavButtons );
+                        return false;
+                    });
+                }
+
+                methods.createNavButtons();
+            },
+
+            goTo: function(slide){
+                if(slide >= settings.total){
+                    settings.current = 0;
+                }else if(slide < 0 ){
+                    settings.current = settings.total - 1;
+                }else{
+                    settings.current = slide;
+                }
+
+                methods.markActive(settings.current);
+                settings.$slider.find('ul').animate({ marginLeft : -settings.current*settings.elementWidth});
+            },
+
+            markActive: function(slide){
+                settings.$slider.find('.nav-buttons span').removeClass('active');
+                settings.$slider.find('.nav-buttons span').eq(slide).addClass('active');   
+            },
+
+            load: function(targetUrl, callback){
+                $.ajax({
+                    type: "GET",
+                    url: targetUrl,
+                    cache: false
+                }).success(function( response ) {
+                    // fade out and remove current list elements
+                    // fade in new elements, recount them and recreate the bullet buttons
+                    settings.$slider.find('ul').fadeOut(function() {
+                        $(this).css('margin-left','0').empty().append( response ).fadeIn();
+                        callback();
+                    });
+                });                
+            },
+
+            createNavButtons: function() {
+                // delete all buttons
+                settings.$slider.find( settings.navButtons ).empty();
+
+                // buttons for every slide
+                var elements = settings.$slider.find('li');
+                settings.total = elements.length; //recount slide number
+                elements.each(function( index ) {
+                    $('<span data-slide="' + index + '"></span>').appendTo( settings.$slider.find( settings.navButtons ) );
+                });
+                methods.goTo(0);
+            }
         }
+
+        methods.init();
+
     };
 })( jQuery );
